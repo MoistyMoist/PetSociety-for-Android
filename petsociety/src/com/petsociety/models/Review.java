cceb637adc8c7ec6cad8db003249cbe98d36585b
MoistyBurger <kaiquan88@gmail.com>
2013-11-13 19:13:50 +0800

    completed review model

--- petsociety/src/com/petsociety/models/Review.java
+++ petsociety/src/com/petsociety/models/Review.java
@@ -2,4 +2,79 @@ package com.petsociety.models;
 
 public class Review {
 
+	private int reviewID;
+	private String description;
+	private String title;
+	private String likes;
+	private String dislikes;
+	private String dateCreated;
+	private String timeCreated;
+	
+	private int locationID;
+	
+	private User user;
+	private Stray stray;
+	
+	
+	public int getReviewID() {
+		return reviewID;
+	}
+	public void setReviewID(int reviewID) {
+		this.reviewID = reviewID;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getLikes() {
+		return likes;
+	}
+	public void setLikes(String likes) {
+		this.likes = likes;
+	}
+	public String getDislikes() {
+		return dislikes;
+	}
+	public void setDislikes(String dislikes) {
+		this.dislikes = dislikes;
+	}
+	public String getDateCreated() {
+		return dateCreated;
+	}
+	public void setDateCreated(String dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+	public String getTimeCreated() {
+		return timeCreated;
+	}
+	public void setTimeCreated(String timeCreated) {
+		this.timeCreated = timeCreated;
+	}
+	public int getLocationID() {
+		return locationID;
+	}
+	public void setLocationID(int locationID) {
+		this.locationID = locationID;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public Stray getStray() {
+		return stray;
+	}
+	public void setStray(Stray stray) {
+		this.stray = stray;
+	}
+	
 }
