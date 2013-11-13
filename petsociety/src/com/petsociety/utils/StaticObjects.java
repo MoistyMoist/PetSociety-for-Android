9a59b37d7595df3a393adaef2cabd169112895c2
MoistyBurger <kaiquan88@gmail.com>
2013-11-13 18:20:31 +0800

    added some static object varables

--- petsociety/src/com/petsociety/utils/StaticObjects.java
+++ petsociety/src/com/petsociety/utils/StaticObjects.java
@@ -1,5 +1,14 @@
 package com.petsociety.utils;
+import com.petsociety.models.*;;
 
 public class StaticObjects {
-
+	
+	final static String token="";
+	
+	//============================//
+	//	  Objects for Parameters  //
+	//============================//
+	static User userToCreate;
+	static User userToLogin;
+	static Location locationToCreate;
 }
