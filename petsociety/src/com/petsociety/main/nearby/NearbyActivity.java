050a75ec6832980ce3ee6666560b47d7fc4dd6e9
mohamad shahrikin b mohamad alias <shahrikinalias@gmail.com>
2013-11-25 09:49:24 +0800

    tryoutMap

--- petsociety/src/com/petsociety/main/nearby/NearbyActivity.java
+++ petsociety/src/com/petsociety/main/nearby/NearbyActivity.java
@@ -3,13 +3,26 @@ package com.petsociety.main.nearby;
 import com.example.petsociety.R;
 import com.example.petsociety.R.layout;
 import com.example.petsociety.R.menu;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.GoogleMap;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
 
 public class NearbyActivity extends Activity {
 
+	private GoogleMap mMap;
+	Button buttonMap;
+    private LocationClient mLocationClient;
+    private static final LocationRequest REQUEST = LocationRequest.create()
+            .setInterval(5000)         // 5 seconds
+            .setFastestInterval(16)    // 16ms = 60fps
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
