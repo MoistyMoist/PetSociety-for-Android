e681e6f0369294e62083c83d85762d8517a74fb6
MoistyMoist <mr.lawrance@outlook.coml.com>
2013-12-08 07:21:44 +0800

    more api.. i will continue after slping...

---
+++ petsociety/src/com/petsociety/httprequests/RetrieveStrayByDateTimeSeenRequest.java
@@ -0,0 +1,63 @@
+package com.petsociety.httprequests;
+
+import java.io.IOException;
+import java.sql.Date;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+
+import android.util.Log;
+
+import com.petsociety.utils.JSONExtractor;
+import com.petsociety.utils.StaticObjects;
+
+public class RetrieveStrayByDateTimeSeenRequest implements Runnable{
+
+	private StaticObjects staticObjects;
+	private Date dateTimeSeen;
+	
+	public RetrieveStrayByDateTimeSeenRequest(Date dateTimeSeen)
+	{
+		this.dateTimeSeen=dateTimeSeen;
+		staticObjects= new StaticObjects();
+	}
+	
+	@Override
+	public void run() {
+
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpGet httpget = null;
+		
+		//PREPARE REQUEST OBJECT
+		httpget = new HttpGet("http://petsociety.cloudapp.net/api/RetrieveStray?INtoken="+staticObjects.getToken()+"&INdateTimeSeen="+this.dateTimeSeen.toString()); 
+
+        Log.i("RETRIEVE STRAY :",httpget.getURI().toString());
+        
+        
+        
+        
+        //EXCUTE REQUEST
+        HttpResponse response;
+        try {
+            response = httpclient.execute(httpget);
+            //PRINT OUT THE RESPONSE
+            Log.i("RETRIEVE STRAY RESPONSE :",response.getStatusLine().toString());
+            //PASS THE RESPONSE TO THE EXTRACTOR
+            JSONExtractor paser= new JSONExtractor();
+            paser.ExtractStrayRequest(response);
+
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+
---
+++ petsociety/src/com/petsociety/httprequests/RetrieveStrayByUserRequest.java
@@ -0,0 +1,61 @@
+package com.petsociety.httprequests;
+
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+
+import android.util.Log;
+
+import com.petsociety.utils.JSONExtractor;
+import com.petsociety.utils.StaticObjects;
+
+public class RetrieveStrayByUserRequest implements Runnable{
+
+	private StaticObjects staticObjects;
+	private int userID;
+	
+	public RetrieveStrayByUserRequest(int userID)
+	{
+		this.userID=userID;
+		staticObjects= new StaticObjects();
+	}
+	
+	@Override
+	public void run() {
+
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpGet httpget = null;
+		
+		//PREPARE REQUEST OBJECT
+		httpget = new HttpGet("http://petsociety.cloudapp.net/api/RetrieveStray?INtoken="+staticObjects.getToken()+"&INuserID="+this.userID); 
+
+        Log.i("RETRIEVE STRAY :",httpget.getURI().toString());
+        
+        
+        
+        
+        //EXCUTE REQUEST
+        HttpResponse response;
+        try {
+            response = httpclient.execute(httpget);
+            //PRINT OUT THE RESPONSE
+            Log.i("RETRIEVE STRAY RESPONSE :",response.getStatusLine().toString());
+            //PASS THE RESPONSE TO THE EXTRACTOR
+            JSONExtractor paser= new JSONExtractor();
+            paser.ExtractStrayRequest(response);
+
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
