efb01c6c5c2b216cc52d07be07690cfa9c66ffec
Euru <Euru@172.27.181.30>
2014-01-15 13:59:19 +0800

    eventlist

--- petsociety/src/com/petsociety/main/profile/CreateEvent.java
+++ petsociety/src/com/petsociety/main/profile/CreateEvent.java
@@ -1,26 +1,138 @@
 package com.petsociety.main.profile;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import com.example.petsociety.R;
 import com.example.petsociety.R.layout;
 import com.example.petsociety.R.menu;
+import com.petsociety.httprequests.LoginRequest;
+import com.petsociety.main.MainActivity;
+import com.petsociety.main.lost.LostProfileActivity;
+import com.petsociety.utils.StaticObjects;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class CreateEvent extends Activity {
+public class CreateEvent extends Activity implements Runnable {
+
+	Button b_create;
+	EditText et_eventTitle, et_description, et_location;
+	TimePicker timePicker1,timePicker2;
+	StaticObjects staticObjects;
+	
+	public CreateEvent(String string, String string2, String string3) {
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_event);
+		
+		b_create = (Button) findViewById(R.id.b_create);
+		et_eventTitle = (EditText) findViewById(R.id.et_eventTitle);
+		et_description = (EditText) findViewById(R.id.et_description);
+		et_location = (EditText) findViewById(R.id.et_location);
+		timePicker1 = (TimePicker) findViewById(R.id.timePicker1);
+		timePicker2 = (TimePicker) findViewById(R.id.timePicker2);
+		
+		
+		b_create.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+			}
+			
+		});
+	}
+	
+	public void checkEvent() {
+
+		et_eventTitle = (EditText) findViewById(R.id.et_eventTitle);
+		et_description = (EditText) findViewById(R.id.et_description);
+		et_location = (EditText) findViewById(R.id.et_location);
+		timePicker1 = (TimePicker) findViewById(R.id.timePicker1);
+		timePicker2 = (TimePicker) findViewById(R.id.timePicker2);
+			
+		
+		 if (et_eventTitle.getText().toString().isEmpty()
+				|| et_description.getText().toString().isEmpty()|| et_location.getText().toString().isEmpty())
+			
+		{
+			Toast.makeText(
+					CreateEvent.this,
+					"Please fill in the empty fields",
+					Toast.LENGTH_LONG).show();
+		}
+		 else{
+			 
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				ExecutorService executor = Executors.newFixedThreadPool(1);
+
+				CreateEvent createEventRequest = new CreateEvent(
+						et_eventTitle.getText().toString(), et_description.getText()
+								.toString(), et_location.getText().toString());
+
+				executor.execute(createEventRequest);
+				executor.shutdown();
+				try {
+					executor.awaitTermination(Long.MAX_VALUE,
+							TimeUnit.NANOSECONDS);
+					Log.i(" RESPONSE :", "ENDED REQUEST");
+
+				} catch (InterruptedException e) {
+				}
+
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						staticObjects = new StaticObjects();
+						if (StaticObjects.getCurrentUser() == null) {
+							Log.i("USER", "NO USER");
+						} else {
+							// User user = StaticObjects.getCurrentUser();
+							// if(et_pass.getText().toString().equals(user.getPassword())){
+							// validUser=true;
+							// }
+						}
+					}
+				});
+			}
+		}).start();
+		Intent intent = new Intent();
+		intent.setClass(getBaseContext(), CreateEvent.class);
+		startActivity(intent);
+		finish();
 	}
 
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.create_event, menu);
 		return true;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- petsociety/src/com/petsociety/main/profile/EventList.java
+++ petsociety/src/com/petsociety/main/profile/EventList.java
@@ -1,22 +1,123 @@
 package com.petsociety.main.profile;
 
+import java.util.List;
+
 import com.example.petsociety.R;
 import com.example.petsociety.R.layout;
 import com.example.petsociety.R.menu;
+import com.petsociety.httprequests.RetrieveAllEventRequest;
+import com.petsociety.main.MainBaseActivity;
+import com.petsociety.main.lost.LostProfileActivity;
+import com.petsociety.main.lost.LostActivity.LostListAdapter;
+import com.petsociety.models.Event;
+import com.petsociety.models.Lost;
+import com.petsociety.models.Pet;
+import com.petsociety.utils.StaticObjects;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class EventList extends Activity {
-
+	
 	ListView lv_events;
+	EventListAdapter adapter;
+	StaticObjects staticObjects;
+	ProgressDialog progress;
+	Context context = this;
 
+	List<Event> eventList;
+
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_event_list);
+		
+		lv_events = (ListView) findViewById(R.id.lv_all_events);
+
+		RetrieveAllEventRequest createAllEvents = new RetrieveAllEventRequest();
+		GetEventList.execute(createAllEvents);
+		
+		lv_events.setOnItemClickListener(new OnItemClickListener(){
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent();
+				intent.setClass(getBaseContext(), EventActivity.class);
+				startActivity(intent);
+			}});
+	}
+	
+private class GetEventList extends AsyncTask<Runnable, Integer, Long> {
+	    
+		@Override
+		protected void onPostExecute(Long result) {
+			
+			super.onPostExecute(result);
+			if(progress!=null)
+				progress.dismiss();
+			fillEventList();
+		}
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+		}
+
+		@Override
+		protected Long doInBackground(Runnable... task) {
+			
+			for(int i=0; i<task.length;i++)
+			{
+				if(task[i]!=null)
+					task[i].run();
+				if (isCancelled()) break;
+			}
+			return null;
+		}
+	 }
+
+	private void fillEventList(){
+		adapter = new EventListAdapter(context);
+		for (int i=0; i<eventList.size(); i++){
+            adapter.add(eventList.get(i));
+		}
+	    lv_events.setAdapter(adapter);
+    }  
+	
+	public class EventListAdapter extends ArrayAdapter<Event> {
+
+		public EventListAdapter(Context context) {
+			super(context, 0);
+		}
+
+		public View getView(int position, View convertView, ViewGroup parent) {
+			if (convertView == null) {
+				convertView = LayoutInflater.from(getContext()).inflate(R.layout.custom_event_row, null);
+			}
+			TextView title = (TextView) convertView.findViewById(R.id.row_event_title);
+			title.setText(getItem(position).getName());
+			TextView desc = (TextView) convertView.findViewById(R.id.row_event_description);
+			desc.setText(getItem(position).getDescription());	
+			TextView date = (TextView) convertView.findViewById(R.id.row_event_date);
+			date.setText(getItem(position).getName());	
+			return convertView;
+		}
+
 	}
 
 	@Override
