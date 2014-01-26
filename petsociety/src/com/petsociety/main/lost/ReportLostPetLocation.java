3ec1db2ee469158a43b6dadb948c21c29a5a96ad
eddietcs <eddietcs@hotmail.com>
2014-01-26 19:26:41 +0800

    lost map

--- petsociety/src/com/petsociety/main/lost/ReportLostPetLocation.java
+++ petsociety/src/com/petsociety/main/lost/ReportLostPetLocation.java
@@ -1,7 +1,7 @@
 package com.petsociety.main.lost;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMap.OnCameraChangeListener;
 import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -9,15 +9,22 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 
 import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 import com.example.petsociety.R;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 /**
  * This shows how to listen to some {@link GoogleMap} events.
  */
-public class ReportLostPetLocation extends FragmentActivity
+public class ReportLostPetLocation extends SherlockFragmentActivity 
         implements OnMapClickListener {
 
     private GoogleMap mMap;
@@ -25,23 +32,50 @@ public class ReportLostPetLocation extends FragmentActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.basic_map);
+        setContentView(R.layout.activity_lost_location);
 
+        ViewGroup viewGroup = (ViewGroup) findViewById(R.id.lost_location_map);
+		viewGroup.addView(View.inflate(this, R.layout.basic_map, null));
         setUpMapIfNeeded();
     }
+    
+    
+	@Override
+	public boolean onCreateOptionsMenu(com.actionbarsherlock.view.Menu menu) {
+		getSupportMenuInflater().inflate(R.menu.lost_location, menu);
+		return true;
+	}
+	
+	
 
-    @Override
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		//return super.onOptionsItemSelected(item);
+		Toast.makeText(getApplicationContext(), "Done", Toast.LENGTH_SHORT).show();
+		return true;
+	}
+
+
+	@Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
     }
 
-    private void setUpMapIfNeeded() {
+	private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
             mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
                     .getMap();
+            // Check if we were successful in obtaining the map.
             if (mMap != null) {
-                setUpMap();
+                //setUpMap();
+                //mMap.setMyLocationEnabled(true);
+                //mMap.setOnMyLocationButtonClickListener(this);
+                LatLng singapore = new LatLng(1.37, 103.84);
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(singapore, 11));
             }
         }
     }
