d42a8ec34ab4255d32b062546ce7f119b1cd21ef
Euru <Euru@172.27.176.90>
2014-01-26 19:31:33 +0800

    eventttttttttttttttt

--- petsociety/src/com/petsociety/main/profile/EventActivity.java
+++ petsociety/src/com/petsociety/main/profile/EventActivity.java
@@ -5,6 +5,7 @@ import com.example.petsociety.R.layout;
 import com.example.petsociety.R.string;
 import com.petsociety.main.MainBaseActivity;
 import com.petsociety.main.profile.EventList.EventListAdapter;
+import com.petsociety.models.Event;
 import com.petsociety.utils.StaticObjects;
 
 import android.os.Bundle;
@@ -16,14 +17,12 @@ import android.widget.Toast;
 
 public class EventActivity extends MainBaseActivity {
 
+	TextView name, desc, time, location;
+
 	public EventActivity() {
 		super(R.string.app_name);
 		// TODO Auto-generated constructor stub
 	}
-
-	EventListAdapter adapter;
-	TextView name, desc, time, location;
-
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -32,14 +31,24 @@ public class EventActivity extends MainBaseActivity {
 		Bundle extras = getIntent().getExtras();
 		int eventID = extras.getInt("event");
 		
-		name = (TextView) findViewById(R.id.tv_name);
-		desc = (TextView) findViewById(R.id.tv_description);
-		time = (TextView) findViewById(R.id.tv_time);
-		location = (TextView) findViewById(R.id.tv_location);
+		TextView name = (TextView) findViewById(R.id.tv_name);
+		TextView desc = (TextView) findViewById(R.id.tv_description);
+		TextView time = (TextView) findViewById(R.id.tv_time);
+		TextView location = (TextView) findViewById(R.id.tv_location);
 
+		Event oneEvent = null;
+		
 		for (int i=0; i<StaticObjects.getEvents().size(); i++){
-            adapter.add(StaticObjects.getEvents().get(i));
+			if(eventID == StaticObjects.getEvents().get(i).getEventID())
+			{
+				oneEvent = StaticObjects.getEvents().get(i);
+			}
+
 		}		
+		
+	    name.setText(oneEvent.getName());
+	    desc.setText(oneEvent.getDescription());
+	   // time.setText(oneEvent.getDateTimeCreated().toString());
 		
 	}
 
