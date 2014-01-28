5f90225f2c8c18732ea5bc65a52a257b312602c4
MoistyMoist <mr.lawrance@outlook.coml.com>
2014-01-28 12:31:11 +0800

    added search activity

---
+++ petsociety/src/com/petsociety/main/Activity_search_address.java
@@ -0,0 +1,26 @@
+package com.petsociety.main;
+
+import com.example.petsociety.R;
+import com.example.petsociety.R.layout;
+import com.example.petsociety.R.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Activity_search_address extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_search_address);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_search_address, menu);
+		return true;
+	}
+
+}
