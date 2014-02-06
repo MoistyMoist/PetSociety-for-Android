3055fd9a028b9f4fdbb5469bb549549c922b57ba
Euru <Euru@172.20.172.149>
2014-02-06 15:29:34 +0800

    profile

--- petsociety/src/com/petsociety/main/profile/ProfileActivity.java
+++ petsociety/src/com/petsociety/main/profile/ProfileActivity.java
@@ -1,19 +1,45 @@
 package com.petsociety.main.profile;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import com.example.petsociety.R;
 import com.example.petsociety.R.layout;
 import com.example.petsociety.R.menu;
+import com.petsociety.httprequests.RetrieveAllEventRequest;
+import com.petsociety.utils.StaticObjects;
 
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ProfileActivity extends Activity {
+		
+	TextView name, location;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_profile);
+		
+		name = (TextView) findViewById(R.id.profile_name);
+		location = (TextView) findViewById(R.id.profile_location);
+
+	    name.setText(StaticObjects.getCurrentUser().getName());
+	    double x = (StaticObjects.getCurrentUser().getX());
+	    double y = (StaticObjects.getCurrentUser().getY());
+	    getMyLocationAddress(x, y);
+
+
 	}
 
 	@Override
@@ -22,5 +48,37 @@ public class ProfileActivity extends Activity {
 		getMenuInflater().inflate(R.menu.profile, menu);
 		return true;
 	}
-
+	
+	public void getMyLocationAddress(double x, double y) {
+        
+        Geocoder geocoder= new Geocoder(this, Locale.ENGLISH);
+         
+        try {
+              //Place your latitude and longitude
+              List<Address> addresses = geocoder.getFromLocation(x, y, 1);
+              if(addresses != null) {
+               
+                  Address fetchedAddress = addresses.get(0);
+                  StringBuilder strAddress = new StringBuilder();
+                
+                  for(int i=0; i<fetchedAddress.getMaxAddressLineIndex(); i++) {
+                        strAddress.append(fetchedAddress.getAddressLine(i)).append("\n");
+                        Log.i("Address", fetchedAddress.getAddressLine(i));
+                  }
+                  
+        	    	location.setText(strAddress.toString());
+              }
+               
+              else{
+            	  location.setText("No location found..!");
+              }
+          
+        } 
+        catch (IOException e) {
+                 // TODO Auto-generated catch block
+                 e.printStackTrace();
+                 Toast.makeText(getApplicationContext(),"Could not get address..!", Toast.LENGTH_LONG).show();
+        }
+    }
+	
 }
